f9ddac051cb4b818aae7a3cd1e2a7324eecd761e
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-03-21 03:53:14 +0200

    final

---
+++ TempConv.cpp
@@ -0,0 +1,13 @@
+#include "TempConv.h"
+
+
+using namespace System;
+using namespace System::Windows::Forms;
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	TemperatureConvertor::TempConv form;
+	Application::Run(% form);
+
+}
